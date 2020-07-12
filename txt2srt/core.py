603cfa8ac6f3ac16ab02e72d53e5c56f610ba9af
Allison Casey <alliecasey21@gmail.com>
2020-07-12 16:36:08 -0400

    refactor int div

--- txt2srt/core.py
+++ txt2srt/core.py
@@ -99,11 +99,11 @@ def convert(src_txt: str, timedelta: int = 5) -> str:
         output.append(str(i))
         time = i * timedelta
         nexttime = (i + 1) * timedelta
-        hour = int(time / 3600)
-        min = int(time / 60) % 60
+        hour = time // 3600
+        min = time // 60 % 60
         sec = int(time) % 60
-        nexthour = int(nexttime / 3600)
-        nextmin = int(nexttime / 60) % 60
+        nexthour = nexttime // 3600
+        nextmin = nexttime // 60 % 60
         nextsec = int(nexttime) % 60
         output.append(
             "%02d:%02d:%02d,000 --> %02d:%02d:%02d,000"
@@ -171,43 +171,3 @@ if __name__ == "__main__":
 
     tk.Button(window, text="Convert", command=run).grid(row=2, column=6, columnspan=3)
     window.mainloop()
-
-
-# exportf = sys.argv[2]
-# importf = sys.argv[1]
-# if len(sys.argv) - 1 == 3:
-# maxmins, maxsecs = sys.argv[3].split(":")
-# maxtime = int(maxmins) * 60 + int(maxsecs)
-# else:
-# maxtime = 0
-
-# try:
-# file = open(importf, "r")
-# except:
-# print("Error: file not found")
-
-# numlines = sum(1 if line != "\n" else 0 for line in open(importf, "r"))
-
-# if maxtime != 0:
-# timedelta = maxtime / numlines
-# else:
-# timedelta = 2
-
-# outfile = open(exportf, "w")
-# i = 0
-# for x in file:
-# if x == "\n":
-# continue
-# outfile.write(str(i) + "\n")
-# time = i * timedelta
-# nexttime = (i + 1) * timedelta
-# hour = int(time / 3600)
-# min = int(time / 60) % 60
-# sec = int(time) % 60
-# nexthour = int(nexttime / 3600)
-# nextmin = int(nexttime / 60) % 60
-# nextsec = int(nexttime) % 60
-# outfile.write("%02d:%02d:%02d,000 --> %02d:%02d:%02d,000\n" %
-# (hour, min, sec, nexthour, nextmin, nextsec))
-# outfile.write(x + "\n")
-# i += 1
