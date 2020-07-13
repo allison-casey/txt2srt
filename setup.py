e614735f7e2788802cc539fb93ad13697aa6a58c
Allison Casey <alliecasey21@gmail.com>
2020-07-13 00:26:04 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='txt2srt',
+      version='0.1',
+      description='Converts text files to srt file format.',
+      url='https://github.com/robbyblum/txt2srt',
+      license='MIT',
+      packages=['txt2srt'],
+      zip_safe=False)
